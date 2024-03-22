060f9b9c0a77b82f95a1f600943c5264d40e0dba
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-03-22 11:33:33 -0600

    updated

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -12,7 +12,7 @@ export const Categories = () => {
     
     <section className="w-full h-auto bg-red-50 lg:px-20 grid grid-cols-1 sm:grid-cols-2 ">
             <div className="col-span-1 sm:col-span-2 w-full h-[100px] flex justify-center items-center">
-                <h2 className="font-semibold text-xl text-[#ca8a8a] texto ">Características</h2>
+                <h2 className="font-semibold text-xl text-[#ca8a8a] texto ">Nuestros productos</h2>
             </div>
            
         {
